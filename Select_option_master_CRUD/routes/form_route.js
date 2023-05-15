6a265978050ed0364c29365d7fc856f86eff540c
Gajjar Nandani <127747074+Nandani-56@users.noreply.github.com>
2023-05-16 01:07:38 +0530

    Update form_route.js

--- Select_option_master_CRUD/routes/form_route.js
+++ Select_option_master_CRUD/routes/form_route.js
@@ -4,6 +4,7 @@ const route = express.Router();
 
 route.get("/form", formRoute.renderForm);
 
+route.post("/insertMultipleData",formRoute.insertMultipleSelectMasterData);
 
 
 module.exports = route;
